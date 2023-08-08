4bee580e31d70b7a1f9518630315de8048680597
Emir Tolis <121692132+emirtolis@users.noreply.github.com>
2023-08-09 01:46:07 +0300

    Create ready.js

---
+++ events/ready.js
@@ -0,0 +1,33 @@
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v10");
+const { TOKEN } = require("../config.json");
+const { Client, GatewayIntentBits, Partials } = require("discord.js");
+const INTENTS = Object.values(GatewayIntentBits);
+const PARTIALS = Object.values(Partials);
+// Altyapı by: Emir Tolis -yt.com/@emirtolis
+
+const client = new Client({
+    intents: INTENTS,
+    allowedMentions: {
+        parse: ["users"]
+    },
+    partials: PARTIALS,
+    retryLimit: 3
+});
+
+module.exports = async (client) => {
+
+  const rest = new REST({ version: "10" }).setToken(TOKEN || process.env.token);
+  try {
+    await rest.put(Routes.applicationCommands(client.user.id), {
+      body: client.commands,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+
+    console.log(`${client.user.tag} Aktif!`);
+    client.user.setActivity("| Emir Tolis |")
+};
+// Altyapı by: Emir Tolis -yt.com/@emirtolis
+// Altyapı by: Emir Tolis -yt.com/@emirtolis
